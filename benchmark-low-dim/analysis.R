481b9a27d32440d820d5f0373a9ceae317af8e54
samperochkin <samuel.perreault.3@ulaval.ca>
2024-04-30 14:29:53 -0400

    extra sims + modif axis figures

--- benchmark-low-dim/analysis.R
+++ benchmark-low-dim/analysis.R
@@ -38,9 +38,45 @@ p0 <- 2
 times00 <- times0[order(fun, n)][tau == tau0 & type == type0 & p==p0 & fun == fun0]
 diff(times00$ltime)
 
-p0 <- 2
+p0 <- 4
 times00 <- times0[order(fun, n)][tau == tau0 & type == type0 & p==p0 & fun == fun0]
 diff(times00$ltime)
+
+
+gg <- ggplot(times0[p %in% c(2,4) & type == "median runtime" & tau %in% c(0,.5,1)],
+             aes(x = log(n,2), y = log(time,2), linetype=as.factor(p), col=as.factor(fun))) +
+  theme_bw() +
+  theme(aspect.ratio = 1,
+        panel.grid.minor = element_blank(),
+        axis.text=element_text(size=9),
+        # legend.position = "bottom",
+        # legend.box = "horizontal",
+        # legend.direction = "vertical",
+        legend.position = "right",
+        legend.direction = "vertical",
+        legend.background = element_rect(colour="black"),
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size=10, margin = margin(r = 10, unit = "pt")),
+        # legend.spacing.x = unit(.5, "cm"),
+        panel.spacing = unit(0.5, "lines"),
+        strip.text.x = element_text(size = 10)) +
+  guides(col = guide_legend(ncol=2), linetype = guide_legend(ncol=2)) +
+  scale_x_continuous(breaks = seq(6,30,2)) +
+  scale_y_continuous(breaks = seq(-14,14,4)) +
+  xlab(expression(log[2]~n)) +
+  ylab(expression(log[2](median~runtime))) +
+  labs(col="algorithm") +
+  scale_linetype_manual(name = "dimension (p)", values = c(1,2,3,4)) +
+  geom_line(size=.5) +
+  facet_grid(~tau, labeller = label_bquote(cols = "t" == .(tau)))
+gg
+
+# pdf("figures/bench_subres.pdf", height = 6.5/3, width = 6.5)
+# print(gg)
+# dev.off()
+
+
+
 
 fun0 <- "KO"
 p0 <- 2
--- misc/check-slopes.R
+++ misc/check-slopes.R
@@ -1,8 +1,39 @@
-times0[order(fun, p, n)][type == "median runtime" & tau == .5 & fun == "DAC", 
-                         median(log(time,2)), .(fun, p, n)]
+# Script that generates the plots for the benchmark analysis
 
-tt <- times0[order(fun, p, n)][type == "median runtime" & tau == .5 & fun == "DAC", 
-                         median(log(time,2)), .(p, n)] |>
-  dcast(n~p)
+# packages ----------------------------------------------------------------
+library(ggplot2)
+library(data.table)
 
-apply(tt, 2, diff)
+
+
+# load results and format -------------------------------------------------
+times <- list.files("benchmark/", pattern = "times[1-9]", full.names = TRUE) |> lapply(fread) |> rbindlist()
+times0 <- times[, .(mean_time = mean(time), min_time = min(time), max_time = max(time), N = .N),
+                .(n,tau,p,fun)]
+
+times0 <- rbind(
+  times[, .(time = mean(time), type = "mean", N = .N), .(n,tau,p,fun)],
+  times[, .(time = median(time), type = "median", N = .N), .(n,tau,p,fun)],
+  times[, .(time = min(time), type = "min", N = .N), .(n,tau,p,fun)],
+  times[, .(time = max(time), type = "max", N = .N), .(n,tau,p,fun)]
+)
+times0$type <- factor(times0$type,
+                      levels = c("min", "mean", "median", "max"),
+                      labels = c("min. runtime", "mean runtime", "median runtime", "max. runtime"))
+times0[grepl("BF", fun), fun := "BF"]
+times0[grepl("KO", fun), fun := "KO"]
+times0[grepl("KE", fun), fun := "KE"]
+times0[grepl("DAC", fun), fun := "DAC"]
+
+
+
+# tau0 <- .5
+tau0 <- .75
+# times0[order(fun, p, n)][type == "median runtime" & tau == tau0 & fun == "DAC", 
+#                          median(log(time,2)), .(fun, p, n)]
+tt <- times0[order(fun, p, n)][type == "median runtime" & tau == tau0 & fun == "DAC", 
+                         median(log(time,2)), .(p, n)] |> dcast(n~p)
+tt <- cbind(tt$n[-1], log(tt$n,2)[-1], apply(tt[,-1], 2, diff))
+tt
+
+t(apply(tt[,-(1:2)], 1, diff))
